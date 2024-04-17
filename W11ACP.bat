3afe018f6a3378e312e666b5af3f4b4c575d73b0
Yasin <140549630+Symbolexe@users.noreply.github.com>
2024-04-17 18:18:19 +0330

    W11ACP
    run as administrator.

---
+++ W11ACP.bat
@@ -0,0 +1,10 @@
+slmgr.vbs /upk
+slmgr.vbs /cpky
+slmgr.vbs /ckms
+DISM /online /Get-TargetEditions
+sc config LicenseManager start= auto & net start LicenseManager
+sc config wuauserv start= auto & net start wuauserv
+changepk.exe /productkey VK7JG-NPHTM-C97JM-9MPGT-3V66T
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
